3af09eb8bffb35692a30813aef5ad8dcfdcf41e7
Ervin <Geineryodan@gmail.com>
2024-06-16 12:20:54 -0600

    I changed vertion of google-services since since I was still getting the same error

--- lib/main.dart
+++ lib/main.dart
@@ -2,67 +2,28 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:job_finder_flutter/user_state.dart';
 
-
-void main() {
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp( MyApp());
+  
+  // Inicialización asíncrona de Firebase
+  await Firebase.initializeApp();
+  
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot)
-      {
-        if(snapshot.connectionState == ConnectionState.waiting)
-        {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('iJob clone app is being initialized',
-                style: TextStyle(
-                  color: Colors.cyan,
-                  fontSize: 38,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Signatra'
-                ),
-                ),
-              ),
-            ),
-          );
-        }
-        else if(snapshot.hasError)
-        {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('An error has been occurred',
-                  style: TextStyle(
-                      color: Colors.cyan,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'iJob Clone App',
-          theme: ThemeData(
-            scaffoldBackgroundColor: Colors.black,
-            primarySwatch: Colors.blue,
-          ),
-          home: UserState(),
-        );
-      }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'iJob Clone App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        primarySwatch: Colors.blue,
+      ),
+      home: UserState(),
     );
   }
 }
